c67b6e7d923656ab99ec40e8da2807db06b423f2
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-12-22 06:57:53 +0700

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra091222;
+
+/**
+ *
+ * @author HP
+ */
+public class employee extends person
+{
+  public String getName(){
+  System.out.println("Employee Name:" + name); 
+  return name;
+  } 
+}
